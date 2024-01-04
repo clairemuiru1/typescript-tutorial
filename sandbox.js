9340690ab7a3810ca12c304a7a21e0eea5ff74f9
clairemuiru1 <muiruclaire@gmail.com>
2024-01-04 11:15:01 +0300

    "lesson 3"

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var character = 'Mario';
+var age = 20;
+var isBlackBelt = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7.5));
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+let character = 'Mario';
+let age = 20;
+let isBlackBelt = false;
+
+const circ = (diameter :number) =>{
+    return diameter*Math.PI;
+}
+
+console.log(circ(7.5)); 
+
